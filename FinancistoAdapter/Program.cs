0649611997dae6e2fabf7bf40dd22eb854e93704
Vladimir Panchenko <dropsonic.pan@gmail.com>
2023-07-13 13:24:41 +0200

    Formatting&cleanup

--- FinancistoAdapter/Program.cs
+++ FinancistoAdapter/Program.cs
@@ -11,21 +11,22 @@ using FinancistoAdapter.Entities;
 
 string fileName = null;
 string outputFileName;
-string arg = args.Length > 0 && args[0] != null ? args[0] : Environment.CurrentDirectory;
+var arg = args.Length > 0 && args[0] != null ? args[0] : Environment.CurrentDirectory;
+
 if (arg != null)
-{
-	fileName = File.GetAttributes(arg).HasFlag(FileAttributes.Directory) ? Directory.EnumerateFiles(arg, "*.backup").MaxBy(Path.GetFileNameWithoutExtension) : arg;
-}
+    fileName = File.GetAttributes(arg).HasFlag(FileAttributes.Directory)
+        ? Directory.EnumerateFiles(arg, "*.backup").MaxBy(Path.GetFileNameWithoutExtension)
+        : arg;
 
-if (String.IsNullOrEmpty(fileName) || !File.Exists(fileName))
-	Environment.Exit(-1);
+if (string.IsNullOrEmpty(fileName) || !File.Exists(fileName))
+    Environment.Exit(-1);
 if (args.Length > 1 && args[1] != null)
-	outputFileName = args[1];
+    outputFileName = args[1];
 else
-	outputFileName = Path.ChangeExtension(fileName, "csv");
+    outputFileName = Path.ChangeExtension(fileName, "csv");
 
-if (String.IsNullOrEmpty(fileName)) 
-	throw new InvalidOperationException("File name cannot be null or empty.");
+if (string.IsNullOrEmpty(fileName))
+    throw new InvalidOperationException("File name cannot be null or empty.");
 
 using var fileStream = File.OpenRead(fileName);
 using var reader = new BackupReader(fileStream);
@@ -33,97 +34,95 @@ var lines = reader.ReadAll();
 var entities = RecordReader.ReadRecords(lines).ToArray();
 
 var transactions =
-	entities
-		.OfType<Transaction>()
-		.Where(t => t.DateTime >= new DateTime(2015, 12, 1))
-		.Where(t => t.To == null)
-		.Where(t => t.Category != Category.Split)
-		.OrderBy(t => t.DateTime)
-		.ToArray();
+    entities
+        .OfType<Transaction>()
+        .Where(t => t.DateTime >= new DateTime(2015, 12, 1))
+        .Where(t => t.To == null)
+        .Where(t => t.Category != Category.Split)
+        .OrderBy(t => t.DateTime)
+        .ToArray();
 // ReSharper disable UnusedVariable
 var accounts =
-	entities
-		.OfType<Account>()
-		.ToArray();
+    entities
+        .OfType<Account>()
+        .ToArray();
 var currencies =
-	entities
-		.OfType<Currency>()
-		.ToArray();
+    entities
+        .OfType<Currency>()
+        .ToArray();
 var payees =
-	entities
-		.OfType<Payee>()
-		.ToArray();
+    entities
+        .OfType<Payee>()
+        .ToArray();
 var categories =
-	entities
-		.OfType<Category>()
-		.ToArray();
+    entities
+        .OfType<Category>()
+        .ToArray();
 var projects =
-	entities
-		.OfType<Project>()
-		.ToArray();
+    entities
+        .OfType<Project>()
+        .ToArray();
 var attributes =
-	entities
-		.OfType<AttributeDefinition>()
-		.ToArray();
+    entities
+        .OfType<AttributeDefinition>()
+        .ToArray();
 // ReSharper restore UnusedVariable
 var attributeValues =
-	entities
-		.OfType<TransactionAttribute>()
-		.Where(a => a.Attribute != null) // workaround for inconsistent data
-		.ToArray();
+    entities
+        .OfType<TransactionAttribute>()
+        .Where(a => a.Attribute != null) // workaround for inconsistent data
+        .ToArray();
 
 // "За всех" attribute
-var sharedExpenseAttrs = attributeValues.Where(a => String.Equals(a.Attribute.Title, "За всех", StringComparison.OrdinalIgnoreCase))
-	.Select(a => new {a.Transaction, Value = bool.Parse(a.Value ?? "false")})
-	.GroupBy(a => a.Transaction, a => a.Value);
+var sharedExpenseAttrs = attributeValues
+    .Where(a => string.Equals(a.Attribute.Title, "За всех", StringComparison.OrdinalIgnoreCase))
+    .Select(a => new { a.Transaction, Value = bool.Parse(a.Value ?? "false") })
+    .GroupBy(a => a.Transaction, a => a.Value);
 var sharedExpenseMap = new Dictionary<Transaction, bool>();
 
 foreach (var item in sharedExpenseAttrs)
 {
-	bool value = false;
-	foreach (var v in item)
-	{
-		value |= v;
-	}
+    var value = false;
+    foreach (var v in item) value |= v;
 
-	sharedExpenseMap[item.Key] = value;
+    sharedExpenseMap[item.Key] = value;
 }
 
-using (FileStream file = File.Create(outputFileName))
+using (var file = File.Create(outputFileName))
 {
-	using (StreamWriter writer = new StreamWriter(file, Encoding.UTF8))
-	{
-		using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
-		{
-			csv.Context.TypeConverterOptionsCache.GetOptions<DateTime>().Formats = new [] { "yyyy-MM-dd HH:mm:ss" };
+    using (var writer = new StreamWriter(file, Encoding.UTF8))
+    {
+        using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+        {
+            csv.Context.TypeConverterOptionsCache.GetOptions<DateTime>().Formats = new[] { "yyyy-MM-dd HH:mm:ss" };
 
-			//csv.WriteExcelSeparator();
-			csv.WriteField("Date&Time");
-			csv.WriteField("Account");
-			csv.WriteField("Currency");
-			csv.WriteField("Amount");
-			csv.WriteField("Category");
-			csv.WriteField("Payee");
-			csv.WriteField("Project");
-			csv.WriteField("Note");
-			csv.WriteField("Shared Expense");
-			csv.NextRecord();
+            //csv.WriteExcelSeparator();
+            csv.WriteField("Date&Time");
+            csv.WriteField("Account");
+            csv.WriteField("Currency");
+            csv.WriteField("Amount");
+            csv.WriteField("Category");
+            csv.WriteField("Payee");
+            csv.WriteField("Project");
+            csv.WriteField("Note");
+            csv.WriteField("Shared Expense");
+            csv.NextRecord();
 
-			foreach (Transaction tran in transactions)
-			{
-				csv.WriteField(tran.DateTime);
-				csv.WriteField(tran.From?.Title);
-				csv.WriteField(tran.From?.Currency.Name);
-				csv.WriteField(tran.FromAmount?.ToString("0.00"));
-				csv.WriteField(tran.Category?.Title);
-				csv.WriteField(tran.Payee?.Title);
-				csv.WriteField(tran.Project?.Title);
-				csv.WriteField(tran.Note);
-				csv.WriteField(sharedExpenseMap.TryGetValue(tran, out bool value) && value);
-				csv.NextRecord();
-			}
-		}
-	}
+            foreach (var tran in transactions)
+            {
+                csv.WriteField(tran.DateTime);
+                csv.WriteField(tran.From?.Title);
+                csv.WriteField(tran.From?.Currency.Name);
+                csv.WriteField(tran.FromAmount?.ToString("0.00"));
+                csv.WriteField(tran.Category?.Title);
+                csv.WriteField(tran.Payee?.Title);
+                csv.WriteField(tran.Project?.Title);
+                csv.WriteField(tran.Note);
+                csv.WriteField(sharedExpenseMap.TryGetValue(tran, out var value) && value);
+                csv.NextRecord();
+            }
+        }
+    }
 }
 
 Console.WriteLine("Done!");
